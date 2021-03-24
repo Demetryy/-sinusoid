196a2a2cc7425ae07ec3ec12dba6a34aef65f699
Demetryy <61795534+Demetryy@users.noreply.github.com>
2021-03-24 10:39:42 +0300

    Update GenSinus.h

--- GenSinus.h
+++ GenSinus.h
@@ -5,8 +5,8 @@
 class GenSinus
 {
 public:
-    void WriteHeader(ofstream& file, unsigned long sampleRate, unsigned long time);
-    void Generation(ofstream& file, unsigned long sampleRate, unsigned long waveFrequency, unsigned long waveVolume, unsigned long time);
+    void WriteHeader(std::ofstream& file, unsigned long sampleRate, unsigned long time);
+    void Generation(std::ofstream& file, unsigned long sampleRate, unsigned long waveFrequency, unsigned long waveVolume, unsigned long time);
     //void Initialization(unsigned long sampleRate, unsigned long waveFrequency, unsigned long waveVolume, unsigned long time)
     unsigned long sampleRate, waveVolume, waveFrequency, time;
 };
